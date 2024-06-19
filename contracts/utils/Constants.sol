182ae728c19f77885627955a98cecfa4f30a21c1
paulalsop <paulalsop072@gmail.com>
2024-06-19 18:12:48 +0800

    update Constants constant to public

--- contracts/utils/Constants.sol
+++ contracts/utils/Constants.sol
@@ -3,11 +3,11 @@
 pragma solidity ^0.8.0;
 
 library Constants {
-    address constant routerAddress = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address public routerAddress = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
 
-    address constant zeroAddress = 0x0000000000000000000000000000000000000000;
+    address public zeroAddress = 0x0000000000000000000000000000000000000000;
 
-    address constant burnAddress = 0x000000000000000000000000000000000000dEaD;
+    address public burnAddress = 0x000000000000000000000000000000000000dEaD;
 
-    address constant usdtAddress = 0x55d398326f99059fF775485246999027B3197955;
+    address public usdtAddress = 0x55d398326f99059fF775485246999027B3197955;
 }
